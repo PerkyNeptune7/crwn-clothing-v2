49f024e94fb4c32f8e38bfa5a883f4aafa3b12f2
Shitij Aggarwal <156359542+PerkyNeptune7@users.noreply.github.com>
2024-06-11 17:47:10 -0400

    created checkout properly

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Navigation from './components/routes/navigation/navigation.component';
 import Home from "./components/routes/home/home.component";
 import Authentication from './components/routes/authentication/authentication.component';
 import Shop from './components/routes/shop/shop.component';
-
+import Checkout from './components/routes/checkout/checkout.component';
 const App = ()=>{
   return(
     <Routes>
@@ -11,6 +11,7 @@ const App = ()=>{
       <Route index element={<Home />}/>
         <Route path='shop' element={<Shop />}/>
         <Route path='auth' element={<Authentication />}/>
+        <Route path='checkout' element={<Checkout />} />
       </Route>
   </Routes> 
   );
