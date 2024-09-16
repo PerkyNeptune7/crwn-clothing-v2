740ebfe93571c3948d9de660831d6f842b23bd91
Shitij Aggarwal <156359542+PerkyNeptune7@users.noreply.github.com>
2024-09-15 22:20:43 -0400

    Changed card to cart

--- src/components/button/button.styles.jsx
+++ src/components/button/button.styles.jsx
@@ -26,7 +26,7 @@ export const BaseButton = styled.button`
 export const GoogleSignInButton = styled(BaseButton)`
   background-color: #4285f4;
   color: white;
-
+  
   &:hover {
     background-color: #357ae8;
     border: none;
